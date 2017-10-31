06e0276792b4017c0ddca3514675f38e56c4c972
ljj1 <502964379@qq.com>
2017-10-31 11:37:04 +0800

    例子一注释测试

--- kotlinx-coroutines-core/src/test/kotlin/guide/example-basic-01.kt
+++ kotlinx-coroutines-core/src/test/kotlin/guide/example-basic-01.kt
@@ -20,10 +20,16 @@ package guide.basic.example01
 import kotlinx.coroutines.experimental.*
 
 fun main(args: Array<String>) {
+    //线程非阻塞延迟1秒
+    //线程非阻塞:用于该段代码的线程，不会因为该段代码需要延迟而被阻塞
+    //该段代码通过挂起方式进行延迟，而该线程开心得跑了，去执行别的代码或处于休息状态。
     launch(CommonPool) { // create new coroutine in common thread pool
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)--线程非阻塞延迟1秒
         println("World!") // print after delay
     }
     println("Hello,") // main function continues while coroutine is delayed
-    Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+    //阻塞线程延迟2秒
+    //为了使该段代码不再执行，需要让该线程在此死等。
+    //线程作为该延迟的陪葬品。
+    Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive--阻塞线程延迟2秒（java api的做法）
 }
